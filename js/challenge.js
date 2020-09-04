242b3a1029574bf4c5a59208d1f469ca527eeb28
Luca <lucakanyo@protonmail.com>
2020-09-03 21:13:05 -0400

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -4,6 +4,10 @@ let plus = () => document.querySelector("#plus")
 let minus = () => document.querySelector("#minus")
 let heart = () => document.querySelector("#heart")
 let likes = () => document.querySelector("ul.likes")
+let submit = () => document.querySelector("#submit")
+let commentList = () => document.querySelector("#list")
+
+let timeOn = true
 
 let timer = setInterval(function(){counter().innerHTML ++;}, 1000);
 
@@ -14,6 +18,34 @@ let incrementTimer = plus().addEventListener("click", function(){
 let decrementTimer = minus().addEventListener("click", function(){
     counter().innerHTML --;
 });
+
+let stopIt = function pauseTimer(){
+    clearInterval(timer)
+}
+
+pause().addEventListener("click", function(){
+    stopIt()
+    plus().disabled = true
+    minus().disabled = true
+    heart().disabled = true
+    submit().disabled = true
+});
+
+heart().addEventListener("click", function(){
+let li = document.createElement('li')
+li.innerHTML = counter().innerText + " was liked and I have no clue yet how to set the how many times"
+likes().appendChild(li)
+});
+
+submit().addEventListener("click", function(e){
+    e.preventDefault()
+    let textBox = document.querySelector("#comment-input")
+    let comment = document.createElement('p')
+    comment.innerHTML = textBox.value
+    commentList().appendChild(comment)
+    textBox.value = ""
+})
+
 
 
 
